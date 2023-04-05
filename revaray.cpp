1b13390aea0e2aea1584f364a62165378b0058af
Jatin Kevlani <129257527+JatinGPACE@users.noreply.github.com>
2023-04-05 21:41:04 +0530

    Update revaray.cpp

--- revaray.cpp
+++ revaray.cpp
@@ -1,16 +1,32 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main () {
-	int a[5],i,temp,j;
-	for (i=0;i<5;i++)
-		cin>>a[i];
-//	for(i=0;i<5;i++)
-//		cout << a[i]<<" ";
-	for (i=0;i<2;i++) {
-			 temp =a[i];
-			a[i]=a[5-i-1];
-			a[5-i-1]=temp;
-	}
-	for(i=0;i<5;i++)
-		cout << a[i]<<" ";
+
+int main(){
+    // Using another array :
+    // int array1[] = {1,2,3}, array2[3];
+    // int index1, index2;
+    // for(index1 =0, index2 = 2; index1 < 3; index1++, index2--){
+    //     array2[index2] = array1[index1];
+    // }
+    // cout<<"Original array :"<<endl;
+    // for(index1 = 0; index1 < 3; index1++){
+    //     cout<<array1[index1]<<endl;
+    // }
+    // cout<<"Reversed array :"<<endl;
+    // for(index1 = 0; index1 < 3; index1++){
+    //     cout<<array2[index1]<<endl;
+    // }
+
+    // Without using another array :
+    int array[] = {1,2,3,4,5};
+    int index, temp;
+    for(index = 0; index < 2; index++){
+        temp = array[index];
+        array[index] = array[5 - index - 1];
+        array[5 - index - 1] = temp;
+    }
+    for(index = 0; index < 5; index++){
+        cout<<array[index]<<endl;
+    }
+    return 0;
 }
